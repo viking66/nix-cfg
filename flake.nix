514d78a81bd5212300167e1d0ca4cce06c8182a2
Jason Davidson <jad658@gmail.com>
2025-01-31 18:02:15 -0500

    Fixing some bugs

--- flake.nix
+++ flake.nix
@@ -9,17 +9,16 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, home-manager, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
+    flake-utils.lib.eachDefaultSystem (system: {
       apps.install = {
         type = "app";
-        program = toString (pkgs.writeShellScript "install" ''
-          ${pkgs.home-manager}/bin/home-manager switch --flake .#jason
+        program = toString (nixpkgs.legacyPackages.${system}.writeShellScript "install" ''
+          ${nixpkgs.legacyPackages.${system}.home-manager}/bin/home-manager switch --flake .#jason
         '');
       };
-      homeConfigurations.jason = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+
+      legacyPackages.homeConfigurations.jason = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
         modules = [ ./home/home.nix ];
       };
     });
