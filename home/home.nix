d7db1c6275fde6488421398efad79ba4bb9d2537
Jason Davidson <jad658@gmail.com>
2025-01-31 12:01:27 -0500

    Handle keys

--- home/home.nix
+++ home/home.nix
@@ -14,15 +14,30 @@
     allowUnfree = true;
   };
 
-  # home.file = {
-  #   ".config/ghostty/config".source = ../dotfiles/ghostty.config;
-  #   ".zshrc".source = ../dotfiles/zshrc;
-  #   "bin" = {
-  #     source = ../bin;
-  #     recursive = true;
-  #     executable = true;
-  #   };
-  # };
+  home.file = {
+    ".config/atuin/config.toml".source = ../dotfiles/atuin.config.toml;
+    ".dir_colors".source = ../dotfiles/dir_colors;
+    ".config/fourmolu.yaml".source  = ../dotfiles/fourmolu.yaml;
+    ".config/ghostty/config".source = ../dotfiles/ghostty.config;
+    ".config/git/config".source = ../dotfiles/git.config;
+    ".config/git/ignore".source = ../dotfiles/git.ignore;
+    ".config/helix/config.toml".source = ../dotfiles/helix.config.toml;
+    ".config/helix/languages.toml".source = ../dotfiles/helix.languages.toml;
+    ".config/nix/nix.conf".source = ../dotfiles/nix.conf;
+    ".ssh/config".source = ../dotfiles/ssh.config;
+    ".config/starship.toml".source = ../dotfiles/starship.toml;
+    ".config/tmux/tmux.conf".source = ../dotfiles/tmux.conf;
+    ".zshrc".source = ../dotfiles/zshrc;
+
+    "bin" = {
+      source = ../bin;
+      recursive = true;
+      executable = true;
+    };
+
+    ".ssh/id_ed25519.pub".source = ../keys/id_ed25519.pub;
+    ".ssh/id_rsa.pub".source = ../keys/id_rsa.pub;
+  };
 
   home.packages = with pkgs; [
     age
@@ -41,3 +56,6 @@
     zlib
   ];
 }
+# id_ed25519.age
+# id_rsa.age
+# sops.key.txt.age
